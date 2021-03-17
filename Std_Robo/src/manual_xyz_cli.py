7202262d92f1497589b67210b51978789181f45c
kedus42 <k.mathewos@jacobs-university.de>
2021-03-17 16:48:51 +0100

    Fixed bugs

--- Std_Robo/src/manual_xyz_cli.py
+++ Std_Robo/src/manual_xyz_cli.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Pose
-import sys
+import sys, time
 
 rospy.init_node("manual_gui")
 xyz_pub=rospy.Publisher('pose', Pose, queue_size=1)
 
 while 1:
-    x=input()
-    y=input()
-    z=input()
+    time.sleep(5)
+    x=input("target x:")
+    y=input("target y:")
+    z=input("target z:")
     target=Pose()
-    target.position.x=self.x
-    target.position.y=self.y
-    target.position.z=self.z
+    target.position.x=float(x)
+    target.position.y=float(y)
+    target.position.z=float(z)
     xyz_pub.publish(target)
