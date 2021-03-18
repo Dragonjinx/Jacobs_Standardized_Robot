ed876ad2f3f6a66a7f064d073e0a1a405cf9ed55
kedus42 <k.mathewos@jacobs-university.de>
2021-03-18 21:34:35 +0100

    flexible arduino port assignment added

--- Std_Robo/src/arm_arduino/write_to_arduino.sh
+++ Std_Robo/src/arm_arduino/write_to_arduino.sh
@@ -3,10 +3,10 @@ sudo apt-get install arduino-mk
 cd ~
 mkdir sketchbook
 cd //usr/share/arduino/libraries
-cp ~/catkin_ws/src/sr_pkg/Std_Robo/src/arm_arduino/Servo.h //usr/share/arduino/libraries 
 cp ~/catkin_ws/src/sr_pkg/Std_Robo/src/arm_arduino/arm_arduino.ino ~/sketchbook
 cp ~/catkin_ws/src/sr_pkg/Std_Robo/src/arm_arduino/Makefile ~/sketchbook
 cd ~/sketchbook
 mkdir libraries
-make
+#put the command that grabs the port on the next line
+make PORT=
 make upload clean
