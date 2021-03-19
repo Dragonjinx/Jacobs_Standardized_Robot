bb6fd9926343462f04f85fa455b9a9a713cc1cbb
Dragonjinx <abhilekh1@hotmail.com>
2021-03-19 23:23:40 +0100

    working arduino automation script

--- Std_Robo/src/arm_arduino/write_to_arduino.sh
+++ Std_Robo/src/arm_arduino/write_to_arduino.sh
@@ -1,12 +1,24 @@
-sudo apt-get update
-sudo apt-get install arduino-mk
+#!/bin/bash
 cd ~
 mkdir sketchbook
-cd //usr/share/arduino/libraries
 cp ~/catkin_ws/src/sr_pkg/Std_Robo/src/arm_arduino/arm_arduino.ino ~/sketchbook
 cp ~/catkin_ws/src/sr_pkg/Std_Robo/src/arm_arduino/Makefile ~/sketchbook
 cd ~/sketchbook
 mkdir libraries
+
 #put the command that grabs the port on the next line
-make PORT=
-make upload clean
+
+while getopts ":d:" opt; do
+    case "${opt}" in
+        d) ard_port=${OPTARG} ;;
+        *) echo "Please use the -d tag to pass through arduino port" ;;
+    esac
+done
+
+if [ "${ard_port}" ]; then
+    echo "Uploading code to device at ${ard_port}";
+    make PORT= "$ard_port";
+    make upload clean;
+    exit 0
+fi
+echo "Please use the -d tag to pass through arduino port"
